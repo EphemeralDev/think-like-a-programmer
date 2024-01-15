dd2479bc44a88ff71753a5cffb09b9c45d324d31
EphemeralDev <115334775+EphemeralDev@users.noreply.github.com>
2024-01-15 00:46:41 -0500

    feat: solution to half a square

---
+++ chapter_2/half_a_square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+/*
+
+Write a program that uses only the following output statements:
+ "#" and "\n" to produce the following
+#####
+####
+###
+##
+#
+*/
+
+int main() {
+  for (int row = 1; row <= 5; row++) {
+    for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
+      cout << "#";
+    }
+    cout << "\n";
+  }
+}
